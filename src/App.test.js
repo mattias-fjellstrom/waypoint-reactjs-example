222aa3cf20521dd05eba83900ebc244f299c4f91
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2023-03-07 08:31:55 +0100

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
